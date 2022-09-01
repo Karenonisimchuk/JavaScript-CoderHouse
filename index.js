bd9e520e4ef0cec1dabe2bd733ee35d45ddd7695
Karen Onisimchuk <83030574+Karenonisimchuk@users.noreply.github.com>
2022-09-01 20:50:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
 let seleccionUsuario = parseInt(
     prompt(
-        "Que esta buscando: \n 1.Remera \n 2.Pantalon \n 3.Pollera \n 4.campera \n 5.buzo "
+        "Ingrese cantidad de repeticiones:"
         
     )
 );
 
-switch (seleccionUsuario){
-    case 1:
-        alert("Selecciono una remera")
-    break
-    case 2:
-        alert("Selecciono un pantalon")
-    break
-    case 3:
-        alert("Selecciono una pollera")
-    break
-    case 4:
-        alert("Selecciono una campera")
-    break
-    case 5:
-        alert("Selecciono un buzo")
-    break
-    default:
-        alert("Opcion desconocida")
-        break
+for (let i = 1; i <= seleccionUsuario; i++) {
+    alert('repeticion numero: ' + i);
 }
